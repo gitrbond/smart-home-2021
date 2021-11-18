81497cfcb7883d1c7f81db6eda7aca742cb91d4e
gitrbond <bond2000r@mail.ru>
2021-11-18 17:55:03 +0300

    added empty unit-tests

---
+++ src/test/java/ru/sbt/mipt/oop/TestApplication.java
@@ -0,0 +1,23 @@
+package ru.sbt.mipt.oop;
+
+import org.junit.Test;
+
+//import static junit.framework.Assert.assertEquals;
+
+public class TestApplication {
+
+    @Test
+    public void testReadWrite() {
+        //assertEquals(0, 0);
+    }
+
+    @Test
+    public void testEventManager() {
+
+    }
+
+    @Test
+    public void testEventHandling() {
+
+    }
+}
